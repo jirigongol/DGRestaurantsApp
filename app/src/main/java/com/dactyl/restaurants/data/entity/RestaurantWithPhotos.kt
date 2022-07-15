f68d3194fc624d90ab027a184866fffd6ea6d47c
Jiri Gongol <jirkagongol@gmail.com>
2022-07-15 10:05:31 +0200

    Add to RestaurantWithPhotos

---
+++ app/src/main/java/com/dactyl/restaurants/data/entity/RestaurantWithPhotos.kt
@@ -0,0 +1,32 @@
+package com.dactyl.restaurants.data.entity
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.dactyl.restaurants.model.Location
+import com.dactyl.restaurants.model.Restaurant
+import com.dactyl.restaurants.model.UserRating
+
+data class RestaurantWithPhotos(
+	@Embedded val restaurantEntity: RestaurantEntity,
+
+	@Relation(
+		parentColumn = "id",
+		entityColumn = "restaurant_id"
+	)
+	val photos: List<PhotoEntity>
+)
+
+fun RestaurantWithPhotos.toDomain() = Restaurant(
+	name = restaurantEntity.name,
+	cuisines = restaurantEntity.cuisines,
+	userRating = UserRating(
+		restaurantEntity.aggregateRating,
+		restaurantEntity.ratingColor
+	),
+	location = Location(
+		restaurantEntity.address,
+		restaurantEntity.latitude,
+		restaurantEntity.longitude
+	),
+	photos = photos.map { it.toDomain() }
+)
