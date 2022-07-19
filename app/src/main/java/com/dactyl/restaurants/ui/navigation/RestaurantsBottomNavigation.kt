5368967df5492590fa8078d29cd9a2880dfbd919
Jiri Gongol <jirkagongol@gmail.com>
2022-07-19 15:00:16 +0200

    Handling Light/Dark mode colors, status bar color, status bar content color.

--- app/src/main/java/com/dactyl/restaurants/ui/navigation/RestaurantsBottomNavigation.kt
+++ app/src/main/java/com/dactyl/restaurants/ui/navigation/RestaurantsBottomNavigation.kt
@@ -1,13 +1,20 @@
 package com.dactyl.restaurants.ui.navigation
 
-
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -17,16 +24,32 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun RestaurantsBottomNavigation(
 	navController: NavHostController = rememberNavController()
-){
+) {
 	val navBackStackEntry by navController.currentBackStackEntryAsState()
 	val currentDestination = navBackStackEntry?.destination
-	BottomNavigation {
+
+
+	BottomNavigation(
+		modifier = Modifier.graphicsLayer {
+			clip = true
+			shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)
+			shadowElevation = 50f
+		},
+		backgroundColor = MaterialTheme.colors.background,
+		contentColor = contentColorFor(MaterialTheme.colors.background)
+	) {
 		listOf(
 			BottomNavigationDestinations.RestaurantsList,
 			BottomNavigationDestinations.RestaurantsMap
 		).forEach { screen ->
 			BottomNavigationItem(
-				icon = { Icon(screen.navIcon, contentDescription = null) },
+				icon = {
+					Icon(
+						screen.navIcon,
+						contentDescription = null,
+						modifier = Modifier.size(24.dp),
+					)
+				},
 				label = { Text(stringResource(screen.navTitleResId)) },
 				selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
 				onClick = {
@@ -37,7 +60,9 @@ fun RestaurantsBottomNavigation(
 						launchSingleTop = true
 						restoreState = true
 					}
-				}
+				},
+				unselectedContentColor = Color.Gray,
+				selectedContentColor = MaterialTheme.colors.primary,
 			)
 		}
 	}
